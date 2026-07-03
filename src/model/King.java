75015aeb1b0509ae44307ccf156bc0fae99687e7
Erick Rocha <erickamil@gmail.com>
2026-07-03 08:58:06 -0300

    Adiciona propriedades e métodos para controlar se o Rei já se movimentou

--- src/model/King.java
+++ src/model/King.java
@@ -1,9 +1,18 @@
 package model;
 
 public class King extends Piece {
+    private boolean hasMoved = false;
 
     public King(Color color, int row, int col, Board board) {
         super(color, row, col, board);
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public void setMoved(boolean moved) {
+        this.hasMoved = moved;
     }
 
     @Override
