a9767ec9ed7d65d120de1f145f512751a331fadc
Erick Rocha <erickamil@gmail.com>
2026-07-03 08:58:37 -0300

    Implementa a lógica de movimentos padrão e capturas simples do Rei

--- src/model/King.java
+++ src/model/King.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class King extends Piece {
     private boolean hasMoved = false;
 
@@ -13,6 +16,24 @@ public class King extends Piece {
 
     public void setMoved(boolean moved) {
         this.hasMoved = moved;
+    }
+
+    @Override
+    public List<Move> getCandidateMoves() {
+        List<Move> moves = new ArrayList<>();
+        int[][] dirs = {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
+                { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
+        };
+        for (int[] d : dirs) {
+            int r = row + d[0], c = col + d[1];
+            if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                if (board.isEmpty(r, c) || board.isOpponentPiece(r, c, color)) {
+                    moves.add(new Move(this, r, c, board.isEmpty(r, c) ? MoveType.NORMAL : MoveType.CAPTURE));
+                }
+            }
+        }
+        return moves;
     }
 
     @Override
