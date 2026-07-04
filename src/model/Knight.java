055a26b66a5b671b045e8e2d79cfaa0d03f243de
Erick Rocha <erickamil@gmail.com>
2026-07-04 11:06:38 -0300

    Adiciona a matriz com as coordenadas de saltos em L do Cavalo

--- src/model/Knight.java
+++ src/model/Knight.java
@@ -1,8 +1,21 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Knight extends Piece {
     public Knight(Color color, int row, int col, Board board) {
         super(color, row, col, board);
+    }
+
+    @Override
+    public List<Move> getCandidateMoves() {
+        List<Move> moves = new ArrayList<>();
+        int[][] jumps = {
+                { 2, 1 }, { 2, -1 }, { -2, 1 }, { -2, -1 },
+                { 1, 2 }, { 1, -2 }, { -1, 2 }, { -1, -2 }
+        };
+        return moves;
     }
 
     @Override
