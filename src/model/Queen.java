ae66d4409d89bd5b2935b63305e028d89532df50
Erick Rocha <erickamil@gmail.com>
2026-07-04 10:37:07 -0300

    Adiciona a lógica de movimentos de longo alcance ortogonais para a Rainha

--- src/model/Queen.java
+++ src/model/Queen.java
@@ -1,8 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queen extends Piece {
     public Queen(Color color, int row, int col, Board board) {
         super(color, row, col, board);
+    }
+
+    @Override
+    public List<Move> getCandidateMoves() {
+        List<Move> moves = new ArrayList<>();
+        // Direções ortogonais (como uma Torre)
+        int[][] dirs = {
+                { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }
+        };
+        for (int[] d : dirs) {
+            int r = row + d[0], c = col + d[1];
+            while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                if (board.isEmpty(r, c)) {
+                    moves.add(new Move(this, r, c, MoveType.NORMAL));
+                } else {
+                    if (board.isOpponentPiece(r, c, color)) {
+                        moves.add(new Move(this, r, c, MoveType.CAPTURE));
+                    }
+                    break;
+                }
+                r += d[0];
+                c += d[1];
+            }
+        }
+        return moves;
     }
 
     @Override
