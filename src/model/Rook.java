fcbe9d74887b63fb4430023a3c0daeb2efa26322
Erick Rocha <erickamil@gmail.com>
2026-07-04 11:16:40 -0300

    Adiciona propriedades e métodos para controlar se a Torre já se movimentou

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -1,9 +1,18 @@
 package model;
 
 public class Rook extends Piece {
+    private boolean hasMoved = false;
 
     public Rook(Color color, int row, int col, Board board) {
         super(color, row, col, board);
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public void setMoved(boolean moved) {
+        this.hasMoved = moved;
     }
 
     @Override
