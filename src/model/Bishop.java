89d90acb88bce7ea18d0911ba5560ed5f4763cec
Erick Rocha <erickamil@gmail.com>
2026-07-04 10:40:46 -0300

    Adiciona a lógica de movimentos deslizantes para as duas primeiras diagonais do Bispo

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -1,8 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bishop extends Piece {
     public Bishop(Color color, int row, int col, Board board) {
         super(color, row, col, board);
+    }
+
+    @Override
+    public List<Move> getCandidateMoves() {
+        List<Move> moves = new ArrayList<>();
+        // Primeiras duas diagonais
+        int[][] dirs = { { 1, 1 }, { 1, -1 } };
+        for (int[] d : dirs) {
+            int r = row + d[0], c = col + d[1];
+            while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                if (board.isEmpty(r, c)) {
+                    moves.add(new Move(this, r, c, MoveType.NORMAL));
+                } else {
+                    if (board.isOpponentPiece(r, c, color)) {
+                        moves.add(new Move(this, r, c, MoveType.CAPTURE));
+                    }
+                    break;
+                }
+                r += d[0];
+                c += d[1];
+            }
+        }
+        return moves;
     }
 
     @Override
