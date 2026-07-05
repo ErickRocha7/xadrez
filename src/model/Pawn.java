96fdb5fb17b64ec35ff8daf6e6405ab35a2fb246
Erick Rocha <erickamil@gmail.com>
2026-07-05 11:54:01 -0300

    Adiciona a lógica de avanço duplo opcional a partir da casa inicial

--- src/model/Pawn.java
+++ src/model/Pawn.java
@@ -12,10 +12,18 @@ public class Pawn extends Piece {
     public List<Move> getCandidateMoves() {
         List<Move> moves = new ArrayList<>();
         int direction = (color == Color.WHITE) ? -1 : 1;
+        int startRow = (color == Color.WHITE) ? 6 : 1;
 
         int oneStep = row + direction;
         if (oneStep >= 0 && oneStep < 8 && board.isEmpty(oneStep, col)) {
             moves.add(new Move(this, oneStep, col, MoveType.NORMAL));
+
+            if (row == startRow) {
+                int twoStep = row + 2 * direction;
+                if (board.isEmpty(twoStep, col)) {
+                    moves.add(new Move(this, twoStep, col, MoveType.NORMAL));
+                }
+            }
         }
 
         return moves;
